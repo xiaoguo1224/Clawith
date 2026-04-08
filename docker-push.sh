29c7fa84cfa7603834acd5eba363ea617959f39d
xiaoguo-home <2728885797@qq.com>
2026-04-08 18:11:05 +0800

    chore(dependencies): add data processing libraries to pyproject.toml
    - Included numpy, pandas, polars, pyarrow, scikit-learn, scipy, statsmodels, and tabulate as dependencies for enhanced data manipulation and analysis capabilities.

---
+++ docker-push.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    # 没有参数：输出错误信息到标准错误流
+    echo "ERROR: 未传入版本号参数！"
+    echo "用法: $0 <版本号>"
+    exit 1
+fi
+
+CURRENT_VERSION=$1
+
+docker tag clawith-frontend:latest 10.0.16.26:5000/tkclaw/clawith-frontend:v${CURRENT_VERSION}
+docker push 10.0.16.26:5000/tkclaw/clawith-frontend:v${CURRENT_VERSION}
+
+
+
+docker tag clawith-backend:latest 10.0.16.26:5000/tkclaw/clawith-backend:v${CURRENT_VERSION}
+docker push 10.0.16.26:5000/tkclaw/clawith-backend:v${CURRENT_VERSION}
